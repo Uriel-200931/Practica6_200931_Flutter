8316d85a62a4e58aff8e358e80f3fec5c1853d3b
uriel-200931 <uriel.maldonadocortez@utxicotepec.edu.mx>
2023-09-17 14:37:37 -0600

    Primer Cambio

---
+++ README.md
@@ -0,0 +1,34 @@
+# practica6_200931 - Widget de Botón Personalizado
+![Banner-de-Twitch-Nubes-Gamer-Chica-Morado.png](https://i.postimg.cc/15q3LFXF/Banner-de-Twitch-Nubes-Gamer-Chica-Morado.png)
+## Información General
+
+- **Creado por:** Uriel Maldonado Cortez
+- **Asignatura:** Desarrollo Movil Integral
+- **Grado:** 10
+- **Grupo:** A
+- **Docente:** MTI. Marco Antonio Ramirez Hernandez
+
+## Descripción de la Aplicación
+Este es un ejemplo simple de un widget de botón personalizado en Flutter. El widget tiene un botón que cambia su texto cuando se presiona.
+
+## Captura de Pantalla
+
+![Captura de Pantalla](./assets/1.jpeg)
+![Captura de Pantalla](./assets/2.jpeg)
+![Captura de Pantalla](./assets/3.jpeg)
+
+## Instalación
+
+Para ejecutar esta aplicación, sigue estos pasos:
+
+1. Asegúrate de tener Flutter y Dart instalados en tu sistema.
+
+2. Clona este repositorio o descarga el código fuente.
+
+3. Abre una terminal en la carpeta del proyecto.
+
+4. Ejecuta `flutter pub get` para obtener las dependencias.
+
+5. Ejecuta `flutter run` para iniciar la aplicación en un emulador o dispositivo físico.
+
+
---
+++ lib/src/button.dart
@@ -0,0 +1,45 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  String textButton = "Ara Ara";
+  int index = 0;
+  List<String> collections = ["YameteKudasai", "AHHHHHHH", "Thanks"];
+
+  void onPressedButton() {
+    setState(() {
+      textButton = collections[index];
+      index = index < collections.length - 1 ? index + 1 : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Stateful Widget"),
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(textButton, style: TextStyle(fontSize: 40.0)),
+                ElevatedButton(
+                  child: Text(
+                    "Tocame !!",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: onPressedButton,
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
